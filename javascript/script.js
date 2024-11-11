f75758d63c3c382ac1324df67ad315214ee851dd
creandocodigolatam <creandocodigolatam@gmail.com>
2024-11-11 11:38:04 -0500

    Delete javascript directory

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-function calcularDescuento() {
-    let precio = parseFloat(document.getElementById("precio").value);
-    let descuento = parseFloat(document.getElementById("descuento").value);
-    const factorDescuento = (100 - descuento) / 100;
-    let precioFinal = precio * factorDescuento;
-
-    document.getElementById("precioFinal").textContent = precioFinal.toFixed(2);
-}
